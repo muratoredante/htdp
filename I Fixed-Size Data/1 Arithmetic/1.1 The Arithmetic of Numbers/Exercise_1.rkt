4d970817ae7b3d1fa951139ab6e85e264f468054
muratoredante <muratoredante@yahoo.com>
2023-07-04 16:49:57 -0300

    Left at 1.5

---
+++ I Fixed-Size Data/1 Arithmetic/1.1 The Arithmetic of Numbers/Exercise_1.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define x 3)
+(define y 4)
+
+(sqrt (+ (* x x) (* y y)))
+
+;Distance formula. Not a function yet.
