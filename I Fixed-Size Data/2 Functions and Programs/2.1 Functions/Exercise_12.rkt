14f2406f7f2cd72625aa35bd738bbeeb60ac4081
muratoredante <muratoredante@yahoo.com>
2023-07-07 19:39:35 -0300

    Left at exercise 24

---
+++ I Fixed-Size Data/2 Functions and Programs/2.1 Functions/Exercise_12.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_12) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (cvolume side-length)
+  (expt side-length 3))
+
+(define (csurface side-length)
+  (* (expt side-length 2) 6))
+
---
+++ I Fixed-Size Data/2 Functions and Programs/2.2 Computing/Exercise_23.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_23) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+(define (string-first s)
+  (substring s 0 1))
+
+; (substring "hello world" 0 1)
+; "h"
+
+(string-first "hello world")
+
+; The stepper did the same.
