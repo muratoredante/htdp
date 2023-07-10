c4134007b81583db9fdc14dd11655846a2089cf7
muratoredante <muratoredante@yahoo.com>
2023-07-10 18:36:04 -0300

    Left at step 2 of the design process

---
+++ I Fixed-Size Data/3 How to Design Programs/3.0 How to Design Programs/Exercise_33.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Exercise_33) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+; The year 2000 problem refers to potential computer errors related to
+; the formatting and storage of calendar data for dates in and after the year 2000.
+
+; Many programs represented four-digit years with only the final two digits,
+; making the year 2000 indistinguishable from 1900.
+; Computer systems' inability to distinguish dates correctly had the potential to bring down worldwide
+; infrastructures for computer reliant industries.
+
+; Contrary to published expectations, few major errors occurred in 2000. Supporters of the Y2K remediation effort argued
+; that this was primarily due to the pre-emptive action of many computer programmers and information technology experts. 
+
